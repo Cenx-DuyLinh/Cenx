453aa7fc2f24dd05457ef0ead87e9e6651fd4b10
Duy Linh <duylinh.tran98@gmail.com>
2023-09-24 14:28:07 +0700

    delete API

--- DACN/ClientTest.py
+++ DACN/ClientTest.py
@@ -1,6 +1,8 @@
 import socket
 import time
+import ntplib
 
+ntp_client = ntplib.NTPClient()
 # HOST = "127.0.0.1"
 HOST = "10.8.0.62"
 PORT = 1194
@@ -10,6 +12,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("Connected")
     while True:
         data = s.recv(8192)
+        ntp_response = ntp_client.request("pool.ntp.org")
+        ntp_time = ntp_response.tx_time
         message, timestamp = data.decode().split("\n")
         print(f"Received: {message}, Delay: {time.time() - float(timestamp)}s")
         time.sleep(0.1)
